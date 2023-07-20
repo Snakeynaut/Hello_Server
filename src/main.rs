d9ea8a722a06fc1853925fef92fd6bdeefe15e33
aminf <snakeyna@student.ubc.ca>
2023-07-20 12:11:05 -0700

    Added single threaded client handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::{
+    fs,
+    net::{TcpListener, TcpStream},
+    io::{prelude::*, BufReader},
+};
+
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let content = fs::read_to_string("hello.html").unwrap();
+    let status_line = "HTTP/1.1 200 OK";
+    let length = content.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{content}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
